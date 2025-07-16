deb30c655162bfae53b4c51d8cede5227b52a1d1
Alex Gonzaga <alexgonzaga1004@gmail.com>
2025-07-16 13:33:33 +0200

    Se ha implementado la función que auto calcula mi edad en la pagina de inicio. Se ha implementado el formulario en la pagina de contacto. Se ha quitado el Modo seguro de main para que no cargue las operaciones dos veces al enviar el mensaje de contacto.

--- alex-portfolio/src/pages/ContactPage.jsx
+++ alex-portfolio/src/pages/ContactPage.jsx
@@ -1,10 +1,77 @@
+import { useForm, ValidationError } from "@formspree/react";
+
 function ContactPage() {
-    //TODO: Implementar el formulario de contacto
+  const [state, handleSubmit] = useForm("movlqkrv");
+  if (state.succeeded) {
+    //Una vez se ha enviado el mensaje le mostramos un modal al usuario donde le indicamos que ha sucedido todo correctamente y le damos la opción de volver al inicio
     return (
-        <section>
-            <h2>Para contactar rellene el formulario</h2>
-        </section>
-    )
+        <>
+            <div
+                className="modal fade show"
+                tabIndex="-1"
+                style={{ display: "block", backgroundColor: "rgba(0,0,0,0.5)" }}
+                aria-modal="true"
+                role="dialog"
+            >
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Mensaje enviado</h5>
+                        </div>
+                        <div className="modal-body">
+                            <p>¡Tu mensaje ha sido enviado correctamente!</p>
+                        </div>
+                        <div className="modal-footer">
+                            <a href="/" className="btn btn-primary">
+                                Volver al inicio
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="modal-backdrop fade show"></div>
+        </>
+    );
+  }
+
+  return (
+    <section className="container mt-5 bg-secondary bg-gradient p-5">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="email">
+            Tu correo:{" "}
+          </label>
+          <input
+            className="form-control"
+            id="email"
+            type="email"
+            name="email"
+            placeholder="name@example.com"
+          />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="message">
+            Escribe aquí tu mensaje:{" "}
+          </label>
+          <textarea className="form-control" id="message" name="message" />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+        </div>
+
+        <div className="mb-3">
+          <button className="btn btn-dark" type="submit" disabled={state.submitting}>
+            {" "}
+            Enviar{" "}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default ContactPage;
--- alex-portfolio/src/pages/HomePage.jsx
+++ alex-portfolio/src/pages/HomePage.jsx
@@ -3,12 +3,22 @@ import CV_Alex from "../assets/CV_Alex_Gonzaga.pdf";
 import ProjectCard from "../components/ProjectCard";
 
 function HomePage() {
+  const currentDate = new Date();
+  const birthDate = new Date("06-09-2004");
+  let age;
+  
+  if (currentDate.getMonth() > birthDate.getMonth()) {
+    age = currentDate.getFullYear() - birthDate.getFullYear();
+  } else {
+    age = (currentDate.getFullYear() - 1) - birthDate.getFullYear();
+  }
+
   return (
     <section className="mt-3">
         <section className="sobreMi container bg-secondary bg-gradient p-5">
           <div className="row align-items-center">
             <div className="col-7">
-              <p className="texto">Hola, me llamo <b>Álex</b> tengo {new Date().getFullYear() - 2004} años vivo en <b>Madrid</b>, soy <b>desarrollador web junior</b>, he estudiado un grado superior de <b>Desarrollo de aplicaciones web</b> y estoy comenzando a realizar proyectos propios especializandome en: <b>php, laravel, react</b> e intentando implementar <b>python</b> a mis proyectos ya que me parece un lenguaje bastante polivalente. </p>
+              <p className="texto">Hola, me llamo <b>Álex</b> tengo { age } años vivo en <b>Madrid</b>, soy <b>desarrollador web junior</b>, he estudiado un grado superior de <b>Desarrollo de aplicaciones web</b> y estoy comenzando a realizar proyectos propios especializandome en: <b>php, laravel, react</b> e intentando implementar <b>python</b> a mis proyectos ya que me parece un lenguaje bastante polivalente. </p>
             </div>
             <div className="col d-flex justify-content-center">
               <img className="foto" src={foto} alt="foto de alex" width={200} height={200} />
